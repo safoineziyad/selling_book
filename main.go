bea33a377d9b3c40183f911dc739eb42e636eed5
Ace120C <145904614+Ace120C@users.noreply.github.com>
2025-04-20 15:41:26 +0100

    Add files via upload

--- main.go
+++ main.go
@@ -4,14 +4,24 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os/exec"
 )
 
 func main() {
+	cmd := exec.Command("php", "-S", "localhost:9000")
+
+	fmt.Println("running PHP server...")
+
+	err := cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+
   fs := http.FileServer(http.Dir("../book_galaxy/"))
   http.Handle("/", fs)
 
   fmt.Println("Listening on Port :8080")
-  err := http.ListenAndServe(":8080", nil)
+  err = http.ListenAndServe(":8080", nil)
   if err != nil {
     log.Fatal(err)
   }
