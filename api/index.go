0ebba19d0bd2c58a07c2528c9694f9ca586199b6
Mr_dev <133367963+safoineziyad@users.noreply.github.com>
2025-06-19 23:35:49 +0100

    Organized project for Vercel deployment

---
+++ api/index.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+"net/http"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+w.WriteHeader(http.StatusOK)
+w.Write([]byte("Form submitted successfully!"))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module selling_book
+
+go 1.23.4
